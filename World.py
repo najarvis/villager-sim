909afb82035bb53ea6990d8e27fd8561764cbf01
MarkusHackspacher <hackspacher@gmx.de>
2015-12-14 20:04:55 +0100

    pep8 changes

--- World.py
+++ World.py
@@ -557,22 +557,22 @@ class World(object):  # Class that stores basically EVERYTHING
                     except IndexError:
                         print a, i, start_tile
                         raise IndexError
-
         return array
-    
+
     def get_vnn_array(self, location, r):
-        """ Stands for Von Neumann Neighborhood. 
-            Simply returns a neighborhood based on the initial location and range r"""
+        """ Stands for Von Neumann Neighborhood.
+            Simply returns a neighborhood based
+            on the initial location and range r"""
         return_array = []
         for row_number in range((2 * r) - 1):
             if row_number >= r:
                 num_in_row = (2 * row_number) - (4 * (row_number - r + 1) - 1)
             else:
                 num_in_row = (2 * row_number) + 1
-        
+
             for cell in range(num_in_row):
-                    
+
                 new_location = (location.x + (cell - math.floor(num_in_row / 2.0)), location.y + (row_number - (r - 1)))
                 return_array.append(Vector2(*new_location))
-                
+
         return return_array
